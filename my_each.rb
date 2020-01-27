5453cdc5d2d7a932d274f83dbec07d100fa93490
Kevin Villalobos <kevinv15v@gmail.com>
2020-01-27 22:35:50 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,14 +1,10 @@
 def my_each(array)
-  if block_given?
-    
-    
+ 
   i = 0
   while i < array.length
 # The "while" method will always return a value of => nil"
     yield(array[i])
     i = i + 1
   end
-else 
-  array
-end
+
 end
